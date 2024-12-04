63a45bc07051cdd5576e5d19a8fc14f6e248ab25
VikramGujar <vikramgujar300@gmail.com>
2024-12-04 17:29:38 +0530

    Random number junrator in java

--- src/com/lab/dec_04/NumberGenerator.java
+++ src/com/lab/dec_04/NumberGenerator.java
@@ -1,12 +1,25 @@
 package com.lab.dec_04;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class NumberGenerator {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the minimum and maximum values for the range.");
+		System.out.print("Enter the minimum value: ");
+		int Min = sc.nextInt();
+		System.out.print("Enter the maximum value: ");
+		int Max = sc.nextInt();
+		
+		System.out.println("Random number is : "+generateRandomNumber(Min, Max));
+		sc.close();
 
 	}
 	public static int generateRandomNumber(int min,int max) {
-		return 0;
+		Random rd = new Random();
+		return rd.nextInt(min, max);
 	}
 }
